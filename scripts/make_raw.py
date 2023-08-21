578af9442ac5daab0c0fc862afc19a6b4e4fe517
Martin Engqvist <martin@enginzyme.com>
2023-08-21 10:03:00 +0200

    move data folder into package root directory to avoid hard-coding path

--- scripts/make_raw.py
+++ scripts/make_raw.py
@@ -1,15 +1,20 @@
 from enzymemap import make_initial
-import pandas as pd
+import enzymemap
 import json
+import os
 
-df, compound_to_smiles = make_initial(file_loc = '../data/brenda_2023_1.txt',
-                                       file_loc_inchi = '../data/brenda_ligands.csv',
-                                       file_loc_chebi = '../data/brenda_ligands_chebi.csv',
-                                       manual_corrections=True)
-df.to_csv("../data/raw_reactions.csv",index=False)
-with open("../data/compound_to_smiles.json", 'w') as f:
-    json.dump(compound_to_smiles,f)
+data_dir = os.path.join(os.path.dirname(enzymemap.__file__), 'data')  # Define the data directory
 
-with open("ec_nums.csv",'w') as f:
-    for ec_num in sorted(list(set(df['EC_NUM']))):
-        print(ec_num,file=f)
+df, compound_to_smiles = make_initial(file_loc = os.path.join(data_dir, 'brenda_2023_1.txt'),  # Load BRENDA data
+                                       file_loc_inchi = os.path.join(data_dir, 'brenda_ligands.csv'),  # Load InChI data
+                                       file_loc_chebi = os.path.join(data_dir, 'brenda_ligands_chebi.csv'),  # Load ChEBI data
+                                       manual_corrections=True)  # Apply manual corrections if necessary
+
+df.to_csv(os.path.join(data_dir, "raw_reactions.csv"),index=False)  # Save the raw reactions data to a CSV file
+
+with open(os.path.join(data_dir, "compound_to_smiles.json"), 'w') as f:  # Open a new JSON file to store compound to SMILES mapping
+    json.dump(compound_to_smiles,f)  # Dump the compound to SMILES mapping into the JSON file
+
+with open(os.path.join(data_dir, "ec_nums.csv"),'w') as f:  # Open a new CSV file to store EC numbers
+    for ec_num in sorted(list(set(df['EC_NUM']))):  # Iterate over the unique EC numbers in the dataframe
+        print(ec_num,file=f)  # Print each EC number to the CSV file
